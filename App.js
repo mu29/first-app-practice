4d412b0b46b75c4d67828c403ce57139de402081
정인중 [N Studio] <mu29@snowcorp.com>
2019-04-04 07:05:29 +0900

    2-1. 상자 부품과, 부품을 꾸며 주는 스타일리스트에 대해 알아봐요.

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.box}>
+          <Text>Open up App.js</Text>
+          <Text>to start working on your app!</Text>
+        </View>
       </View>
     );
   }
@@ -17,5 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    padding: 16,
+    margin: 64,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
   },
 });
